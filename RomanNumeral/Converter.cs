ae2b12b1c1ecae7974bc39b3d9d7c4895ed52cec
paulhoulston <paul.houlston@hotmail.co.uk>
2015-06-30 16:14:07 +0100

    Test for conversion of I now works

--- RomanNumeral/Converter.cs
+++ RomanNumeral/Converter.cs
@@ -10,9 +10,17 @@ namespace RomanNumeral
             public void Then_1_is_returned()
             {
                 var expectedResult = 1;
-                var actualResult = 0;
+                var actualResult = new RomanNumerator().ConvertFrom("I");
                 Assert.AreEqual(expectedResult, actualResult);
             }
+        }
+    }
+
+    public class RomanNumerator
+    {
+        public uint ConvertFrom(string romanNumeral)
+        {
+            return 1;
         }
     }
 }
