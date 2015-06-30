d99ffee994da315e1c03db21c6096928cac2e81c
paulhoulston <paul.houlston@hotmail.co.uk>
2015-06-30 16:52:27 +0100

    Added further tests to check for leading roman numeral to decrement by

--- RomanNumeral/Converter.cs
+++ RomanNumeral/Converter.cs
@@ -46,14 +46,19 @@ namespace RomanNumeral
             }
         }
 
-        public class When_I_have_a_roman_numeral_that_is_prefixed_by_a_single_I_numeral
+        public class When_I_have_a_roman_numeral_that_is_prefixed_by_a_single_numeral_less_than_the_first_numeral
         {
             [TestCase(4, "IV")]
             [TestCase(9, "IX")]
+            [TestCase(5, "VX")]
+            [TestCase(40, "XL")]
             [TestCase(49, "IL")]
             [TestCase(99, "IC")]
+            [TestCase(50, "LC")]
             [TestCase(499, "ID")]
+            [TestCase(400, "CD")]
             [TestCase(999, "IM")]
+            [TestCase(900, "CM")]
             public void Then_the_value_of_the_first_is_decremented_by_one(int expectedResult, string romanNumeral)
             {
                 var actualResult = new RomanNumerator().ConvertFrom(romanNumeral);
