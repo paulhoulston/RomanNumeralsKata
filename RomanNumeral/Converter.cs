6156a07a2f749f5fa320a8dcf5a77c3130e1e5d4
paulhoulston <paul.houlston@hotmail.co.uk>
2015-06-30 16:24:07 +0100

    Added failing test for adding trailing 'I' to roman numeral

--- RomanNumeral/Converter.cs
+++ RomanNumeral/Converter.cs
@@ -8,7 +8,6 @@ namespace RomanNumeral
     {
         public class When_I_have_a_simple_roman_numeral
         {
-            [Test]
             [TestCase(1, "I")]
             [TestCase(5, "V")]
             [TestCase(10, "X")]
@@ -17,6 +16,16 @@ namespace RomanNumeral
             [TestCase(500, "D")]
             [TestCase(1000, "M")]
             public void Then_the_corresponding_integer_value_is_returned(int expectedResult, string romanNumeral)
+            {
+                var actualResult = new RomanNumerator().ConvertFrom(romanNumeral);
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        public class When_I_have_a_roman_numeral_post_fixed_by_a_single_I_roman_numeral
+        {
+            [TestCase(2, "II")]
+            public void Then_the_value_is_one_more_than_the_first_numeral(int expectedResult, string romanNumeral)
             {
                 var actualResult = new RomanNumerator().ConvertFrom(romanNumeral);
                 Assert.AreEqual(expectedResult, actualResult);
