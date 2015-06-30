7832f469c428069bc21637ac47302e609939f280
paulhoulston <paul.houlston@hotmail.co.uk>
2015-06-30 16:10:06 +0100

    Added first failing test to convert I to 1

---
+++ RomanNumeral/Converter.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace RomanNumeral
+{
+    public class Given_I_want_to_convert_a_roman_numeral
+    {
+        public class When_I_have_I
+        {
+            [Test]
+            public void Then_1_is_returned()
+            {
+                var expectedResult = 1;
+                var actualResult = 0;
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+    }
+}
